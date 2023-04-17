533e9f5483971b17e0af24ce4ba7744f2586b623
RockyPraxe <filipio.svanda@gmail.com>
2023-04-17 22:43:49 +0000

    Add code to chcek the answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,16 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName('button');
-   
+
     for (let button of buttons) {
-        button.addEventListener('click', function() {
-            if (this.getAttribute("data-type")=== "submit") {
-                alert("You clicked Submit!");
+        button.addEventListener('click', function () {
+            if (this.getAttribute("data-type") === "submit") {
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
             }
-        } )
+        })
     }
 
     runGame("addition");
@@ -21,26 +21,56 @@ document.addEventListener("DOMContentLoaded", function () {
 /** 
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
-*/
+ */
 function runGame(gameType) {
     //Creates two random number between 1 and 25 
-    let num1 = Math.floor(Math.random() * 25) +1;
-    let num2 = Math.floor(Math.random() * 25) +1;
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
     if (gameType === "addition") {
-        displayAdditionQuestion(num1,num2);
+        displayAdditionQuestion(num1, num2);
     } else {
-        alert (`Unknown game type: ${gameType}`);
+        alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
+}
+
+/**
+ * Check the answer against the first element in
+ * the returned calculateCorectAnswer array
+ */
+function checkAnswer() {
+
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert('Hey! You got it right! :D');
+    } else {
+        alert(`Awww.....You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
     }
 
-
-function checkAnswer() {
+    runGame(calculatedAnswer[1]);
 
 }
 
+/**
+ * Gets the operands (the numbers) and the operator (plus,minus,etc)
+ * directly from the DOM and retusnrs the correct answer.
+ */
 function calculateCorrectAnswer() {
+
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+
+    if (operator === '+') {
+        return [operand1 + operand2, 'addition'];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`
+    }
 
 }
 
