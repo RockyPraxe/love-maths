80112182220ac6597ca8ca1f7ea643130e7fb953
RockyPraxe <filipio.svanda@gmail.com>
2023-04-17 23:26:16 +0000

    Add code to chceck the answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName('button');
 
     for (let button of buttons) {
-        button.addEventListener('click', function () {
+        button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
                 checkAnswer();
             } else {
@@ -41,12 +41,12 @@ function runGame(gameType) {
  */
 function checkAnswer() {
 
-    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
     let calculatedAnswer = calculateCorrectAnswer();
     let isCorrect = userAnswer === calculatedAnswer[0];
 
     if (isCorrect) {
-        alert('Hey! You got it right! :D');
+        alert("Hey! You got it right! :D");
     } else {
         alert(`Awww.....You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
     }
@@ -61,15 +61,15 @@ function checkAnswer() {
  */
 function calculateCorrectAnswer() {
 
-    let operand1 = parseInt(document.getElementById('operand1').innerText);
-    let operand2 = parseInt(document.getElementById('operand2').innerText);
-    let operator = document.getElementById('operator').innerText;
+    let operand1 = parseInt(document.getElementById("operand1").innerText);
+    let operand2 = parseInt(document.getElementById("operand2").innerText);
+    let operator = document.getElementById("operator").innerText;
 
-    if (operator === '+') {
-        return [operand1 + operand2, 'addition'];
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
     } else {
         alert(`Unimplemented operator ${operator}`);
-        throw `Unimplemented operator ${operator}. Aborting!`
+        throw `Unimplemented operator ${operator}. Aborting!`;
     }
 
 }
@@ -84,9 +84,9 @@ function incrementWrongAnswer() {
 
 function displayAdditionQuestion(operand1, operand2) {
 
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = '+';
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 
 }
 
